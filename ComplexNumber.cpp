600ad79cac87e80359f75860018d5d8a6615764e
LeoG123321 <3545gonzalez@gmail.com>
2024-01-25 19:55:03 -0600

    Initialized Functions For Complex Number
    added cpp for Complex Number and initialized all function for Complex number

---
+++ ComplexNumber.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "ComplexNumber.hpp"
+
+using namespace std;
+
+ComplexNumber::ComplexNumber(float realNumber, float imagineNumber) {
+	this->realNumber = realNumber;
+	this->imagineNumber = imagineNumber;
+}
--- ComplexNumber.hpp
+++ ComplexNumber.hpp
@@ -7,12 +7,17 @@ private:
 	float realNumber;
 	float imagineNumber;		//Imaginary Number
 
+public:
+	ComplexNumber(float, float);
 
-
-
-
-
-
-
-
+	ComplexNumber add(ComplexNumber, ComplexNumber);	//Addition
+	ComplexNumber sub(ComplexNumber, ComplexNumber);	//Subtract
+	ComplexNumber mult(ComplexNumber, ComplexNumber);	//Multiply
+	ComplexNumber div(ComplexNumber, ComplexNumber);	//Division
+	
+	ComplexNumber negate(ComplexNumber);				//Negation
+	ComplexNumber mag(ComplexNumber);					//Magnitude
+	ComplexNumber con(ComplexNumber);					//Conjugation
+	ComplexNumber real(ComplexNumber);					//Real Part
+	ComplexNumber imagine(ComplexNumber);				//Imaginary Part
 };
